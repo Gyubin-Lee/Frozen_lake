628f985b7efd38c97782da74611cf93fafba2629
Gyubin-Lee <leegyubin00@gmail.com>
2021-05-20 21:39:04 +0900

    v1.0.0

--- test.py
+++ test.py
@@ -8,12 +8,36 @@ actions = [move_forward, turn_left, turn_right]
 
 thin_ice_blocks = [(1, 3), (1, 4), (1, 5), (1, 6), (2, 6), (3, 6), (4, 6)]
 
-set_speed(3)
+set_speed(10)
 test()
-(x, y), ori, sensor, done = set_map(thin_ice_blocks)
+#(x, y), ori, sensor, done = set_map(thin_ice_blocks)
 
 ##############################################
 #### Copy and paste your step 4 code here ####
 ##############################################
+
+(x, y), ori, sensor, done = reset_map()
+
+while True:
+	if ori == 'N':
+		ori_num = 0
+	elif ori == 'E':
+		ori_num = 1
+	elif ori == 'S':
+		ori_num = 2
+	else:
+		ori_num = 3
+
+	l, f, r = sensor
+
+	action_num = np.argmax(q_table[ori_num][l][f][r])
+	action = actions[action_num]
+	(x, y), ori, sensor, done = action()
+
+	if done:
+		break
+    
+    
+
 raise NotImplementedError
 ##############################################
